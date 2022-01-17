956c43f7a9ecdf4fa5c006f863e1999ea6202cac
MNoxx74 <ampam877@gmail.com>
2022-01-17 22:14:10 +0700

    fix apn issue

--- device.mk
+++ device.mk
@@ -30,6 +30,10 @@ PRODUCT_TARGET_VNDK_VERSION := 30
 # Dynamic partitions setup
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# APN
+PRODUCT_COPY_FILES += \
+    device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
